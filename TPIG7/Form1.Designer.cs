b0ad8e0029b6284bf9fd17c78709e0ecfa4459b5
Nahuelnp <nahuelyad@gmail.com>
2022-06-07 18:46:06 -0300

    Agregar archivos de proyecto.

---
+++ TPIG7/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace TPIG7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(391, 450);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(399, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(401, 450);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
+            this.panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TPIG7/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPIG7
+{
+    public partial class Form1 : Form
+    {
+        private int x, arrowStartX, arrowEndX = -1;
+        private int y, arrowStartY, arrowEndY = -1;
+        private Pen pen = new Pen(Color.Black, 5);
+        private Rectangle rect;
+        private bool pintar = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+
+            pintar = true;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (pintar)
+            {
+                rect = new Rectangle(
+                    Math.Min(e.X, x),
+                    Math.Min(e.Y, y),
+                    (e.X - x),
+                    (e.Y - y));
+
+                panel1.Invalidate();
+                panel1.Refresh();
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            pintar = false;
+
+            x = 0;
+            y = 0;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            using (Graphics g = this.panel1.CreateGraphics())
+            {
+                Pen pen = new Pen(Color.Black, 5);
+
+                g.DrawRectangle(pen, rect);
+
+                pen.Dispose();
+            }
+        }
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            arrowStartX = e.X;
+            arrowStartY = e.Y;
+
+            label1.Text = "Arrow Start: " + arrowStartX + ", " + arrowStartY;
+            pintar = true;
+        }
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            label2.Text = "Arrow End: " + e.X + ", " + e.Y;
+
+
+            if (pintar)
+            {
+
+                panel2.Refresh();
+                pen.StartCap = LineCap.RoundAnchor;
+                pen.EndCap = LineCap.ArrowAnchor;
+                using (Graphics g = this.panel2.CreateGraphics())
+                {
+
+                    g.DrawLine(pen, arrowStartX,
+                    arrowStartY,
+                    e.X,
+                    e.Y);
+
+
+                }
+                panel1.Invalidate();
+
+            }
+
+        }
+
+        private void panel2_MouseUp(object sender, MouseEventArgs e)
+        {
+            arrowEndX = e.X;
+            arrowEndY = e.Y;
+
+            pintar = false;
+            panel2.Refresh();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            using (Graphics g = this.panel2.CreateGraphics())
+            {
+                Pen pen = new Pen(Color.Black, 5);
+                pen.StartCap = LineCap.RoundAnchor;
+                pen.EndCap = LineCap.ArrowAnchor;
+
+                g.DrawLine(pen, arrowStartX,
+                arrowStartY,
+                arrowEndX,
+                arrowEndY);
+
+                pen.Dispose();
+
+            }
+
+        }
+
+
+    }
+}
