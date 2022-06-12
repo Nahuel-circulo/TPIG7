dce01755d5ea7566afcacb9b21b093ad482e89c8
Nahuelnp <nahuelyad@gmail.com>
2022-06-12 13:34:29 -0300

    estructura basica del proyecto

--- TPIG7/Form1.Designer.cs
+++ TPIG7/Form1.Designer.cs
@@ -28,80 +28,228 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsPanel = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.toolsPanel.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // menuStrip1
             // 
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(391, 450);
-            this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
-            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
-            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.editarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1006, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem,
+            this.abrirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // editarToolStripMenuItem
+            // 
+            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.editarToolStripMenuItem.Text = "Editar";
+            // 
+            // toolsPanel
+            // 
+            this.toolsPanel.Controls.Add(this.tableLayoutPanel1);
+            this.toolsPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.toolsPanel.Location = new System.Drawing.Point(0, 24);
+            this.toolsPanel.Name = "toolsPanel";
+            this.toolsPanel.Size = new System.Drawing.Size(133, 549);
+            this.toolsPanel.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(133, 549);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(700, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(772, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
             // 
             // panel2
             // 
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(399, 0);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(133, 24);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(401, 450);
-            this.panel2.TabIndex = 1;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
-            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
-            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
-            this.panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseUp);
+            this.panel2.Size = new System.Drawing.Size(873, 549);
+            this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
             // 
-            // label2
+            // pictureBox5
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(269, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.pictureBox5.Image = global::TPIG7.Properties.Resources.Recurso_1solicitar;
+            this.pictureBox5.Location = new System.Drawing.Point(3, 403);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(127, 94);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
             // 
-            // label1
+            // pictureBox4
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(54, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.pictureBox4.Image = global::TPIG7.Properties.Resources.Recurso_2solicitar1;
+            this.pictureBox4.Location = new System.Drawing.Point(3, 303);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(127, 94);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::TPIG7.Properties.Resources.Recurso_3solicitar;
+            this.pictureBox3.Location = new System.Drawing.Point(3, 203);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(127, 94);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TPIG7.Properties.Resources.Recurso_4solicitar;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 103);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(127, 94);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TPIG7.Properties.Resources.Recurso_5solicitar;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(127, 94);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1006, 573);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolsPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolsPanel.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+        private System.Windows.Forms.Panel toolsPanel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- TPIG7/Form1.cs
+++ TPIG7/Form1.cs
@@ -13,8 +13,8 @@ namespace TPIG7
 {
     public partial class Form1 : Form
     {
-        private int x, arrowStartX, arrowEndX = -1;
-        private int y, arrowStartY, arrowEndY = -1;
+        private int x, arrowStartX, arrowEndX = 0;
+        private int y, arrowStartY, arrowEndY = 0;
         private Pen pen = new Pen(Color.Black, 5);
         private Rectangle rect;
         private bool pintar = false;
@@ -22,6 +22,18 @@ namespace TPIG7
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            using (Graphics g = this.panel2.CreateGraphics())
+            {
+                Pen pen = new Pen(Color.Black, 5);
+
+                g.DrawRectangle(pen, rect);
+
+                pen.Dispose();
+            }
         }
 
         private void panel1_MouseDown(object sender, MouseEventArgs e)
@@ -42,8 +54,8 @@ namespace TPIG7
                     (e.X - x),
                     (e.Y - y));
 
-                panel1.Invalidate();
-                panel1.Refresh();
+                panel2.Invalidate();
+                panel2.Refresh();
             }
         }
 
@@ -57,9 +69,9 @@ namespace TPIG7
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            using (Graphics g = this.panel1.CreateGraphics())
+            using (Graphics g = this.panel2.CreateGraphics())
             {
-                Pen pen = new Pen(Color.Black, 5);
+                Pen pen = new Pen(Color.Black, 3);
 
                 g.DrawRectangle(pen, rect);
 
@@ -67,70 +79,73 @@ namespace TPIG7
             }
         }
 
-        private void panel2_MouseDown(object sender, MouseEventArgs e)
-        {
-            arrowStartX = e.X;
-            arrowStartY = e.Y;
-
-            label1.Text = "Arrow Start: " + arrowStartX + ", " + arrowStartY;
-            pintar = true;
-        }
-
-        private void panel2_MouseMove(object sender, MouseEventArgs e)
-        {
-
-            label2.Text = "Arrow End: " + e.X + ", " + e.Y;
 
 
-            if (pintar)
-            {
+        //private void panel2_MouseDown(object sender, MouseEventArgs e)
+        //{
+        //    arrowStartX = e.X;
+        //    arrowStartY = e.Y;
 
-                panel2.Refresh();
-                pen.StartCap = LineCap.RoundAnchor;
-                pen.EndCap = LineCap.ArrowAnchor;
-                using (Graphics g = this.panel2.CreateGraphics())
-                {
+        //    label1.Text = "Arrow Start: " + arrowStartX + ", " + arrowStartY;
+        //    pintar = true;
+        //}
 
-                    g.DrawLine(pen, arrowStartX,
-                    arrowStartY,
-                    e.X,
-                    e.Y);
+        //private void panel2_MouseMove(object sender, MouseEventArgs e)
+        //{
+
+        //    label2.Text = "Arrow End: " + e.X + ", " + e.Y;
 
 
-                }
-                panel1.Invalidate();
+        //    if (pintar)
+        //    {
 
-            }
+        //        panel2.Refresh();
+        //        pen.StartCap = LineCap.RoundAnchor;
+        //        pen.EndCap = LineCap.ArrowAnchor;
+        //        using (Graphics g = this.panel2.CreateGraphics())
+        //        {
 
-        }
+        //            g.DrawLine(pen, arrowStartX,
+        //            arrowStartY,
+        //            e.X,
+        //            e.Y);
 
-        private void panel2_MouseUp(object sender, MouseEventArgs e)
-        {
-            arrowEndX = e.X;
-            arrowEndY = e.Y;
 
-            pintar = false;
-            panel2.Refresh();
-        }
+        //        }
+        //        panel1.Invalidate();
 
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-            using (Graphics g = this.panel2.CreateGraphics())
-            {
-                Pen pen = new Pen(Color.Black, 5);
-                pen.StartCap = LineCap.RoundAnchor;
-                pen.EndCap = LineCap.ArrowAnchor;
+        //    }
 
-                g.DrawLine(pen, arrowStartX,
-                arrowStartY,
-                arrowEndX,
-                arrowEndY);
+        //}
 
-                pen.Dispose();
+        //private void panel2_MouseUp(object sender, MouseEventArgs e)
+        //{
+        //    arrowEndX = e.X;
+        //    arrowEndY = e.Y;
 
-            }
+        //    pintar = false;
+        //    panel2.Refresh();
+        //}
 
-        }
+        //private void panel2_Paint(object sender, PaintEventArgs e)
+        //{
+        //    using (Graphics g = this.panel2.CreateGraphics())
+        //    {
+        //        Pen pen = new Pen(Color.Black, 5);
+        //        pen.StartCap = LineCap.RoundAnchor;
+        //        pen.EndCap = LineCap.ArrowAnchor;
+
+        //        g.DrawLine(pen, arrowStartX,
+        //        arrowStartY,
+        //        arrowEndX,
+        //        arrowEndY);
+
+        //        pen.Dispose();
+
+        //    }
+
+        //}
+
 
 
     }
